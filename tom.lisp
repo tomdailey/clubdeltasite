d4726f07f465d594b9059be1b5c43aa6b2d78ffb
Tom Dailey <tompdailey@gmail.com>
2021-01-07 16:01:27 -0500

    added initial files

---
+++ tom.lisp
@@ -0,0 +1,6 @@
+<h1>Tom Dailey</h1>
+	<p>I tutor kids who are interested in or need help with math, programming, playing the piano, writing, or reading. I'm also interested in finding ways to use an open-source minefcraft clone, <a href="http://minetest.net">Minetest</a>, to motivate and provide an envirnoment to practice algebra and geometry.</p>
+	<p>To contact me for a lesson, email me at tom@theclubdelta.com.</p>
+	<h4>My Schedule</h4>
+	<p>I prefer to tutor in 45-55 minute increments, weekdays between 1 and 5. If you have any other preferences I'd be happy to accomodate.</p>
+	<iframe src="https://calendar.google.com/calendar/embed?src=c_tdk5b59ae7q9q80986aeebgf4o%40group.calendar.google.com&ctz=America%2FNew_York" style="border: 0" width="800" height="600" frameborder="0" scrolling="no"></iframe>
